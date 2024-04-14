2dd63ba1e8146f9365263301f3f03989c0bb90b4
thanhhung <thanhhungdeveloper@gmail.com>
2024-04-14 14:43:29 +0700

    improment - add react query provider to app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -13,7 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
   );
 }
