48f2840825983c3084fd3b89bd89ab0a86f9c0c3
thanhhung <thanhhungdeveloper@gmail.com>
2024-04-25 15:31:26 +0700

    fix authencation redirect

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,17 +3,17 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { checkauth } from "@/utils/fetch";
 
-export default async function DashBoardLayout({
+export default async function layout({
   children,
 }: {
   children: ReactNode;
 }) {
-  const cookieStore = cookies();
-  const authtoken = cookieStore.get("authtoken");
+  // const cookieStore = await cookies();
+  // const authtoken = cookieStore.get("authtoken");
 
-  let data = { isadmin: false };
-  data = await checkauth(authtoken?.value);
-  if (!data.isadmin) return redirect("/auth/signIn");
+  // let data = { isadmin: false };
+  // data = await checkauth(authtoken?.value);
+  // if (!data.isadmin) return redirect("/auth/signIn");
 
   return <>{children}</>;
 }
--- src/layouts/dashboard/layout.tsx
+++ src/layouts/dashboard/layout.tsx
@@ -2,10 +2,21 @@ import React, { ReactNode } from "react";
 import TopNav from "./topNav";
 import SideNav from "./sideNav";
 import { Box } from "@mui/material";
+import { cookies } from "next/headers";
+import { checkauth } from "@/utils/fetch";
+import { redirect } from "next/navigation";
 interface IProps {
   children: ReactNode;
 }
-const DashBoardLayout = ({ children }: IProps) => {
+
+const DashBoardLayout = async ({ children }: IProps) => {
+  const cookieStore = await cookies();
+  const authtoken = cookieStore.get("authtoken");
+
+  let data = { isadmin: false };
+  data = await checkauth(authtoken?.value);
+  if (!data.isadmin) redirect("/auth/signIn");
+
   return (
     <>
       <TopNav />
