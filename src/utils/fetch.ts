4f5c9ef5f9ee5521488bcbaf5b61515a5fe31933
thanhhung <thanhhungdeveloper@gmail.com>
2024-04-27 20:35:27 +0700

    feat - use fetch instead axios

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -6,113 +6,142 @@ import {
   IGetProductsInfinity,
 } from "@/types/product";
 import { IReview } from "@/types/reviews";
-import axios from "axios";
 
-const server_url = process.env.NEXT_PUBLIC_SERVER_URL;
 let token = "";
 
-export const Fetch = axios.create({
-  baseURL: server_url,
-  headers: {
+class FetchClass {
+  readonly baseUrl = process.env.NEXT_PUBLIC_SERVER_URL || "";
+  headers: HeadersInit | undefined = {
     "Content-Type": "application/json",
-  },
-  withCredentials: true,
-});
+  };
+
+  async get(url: string, headers?: { Authorization?: string }) {
+    return fetch(this.baseUrl + url, {
+      headers: {
+        ...this.headers,
+      },
+      credentials: "include",
+    });
+  }
+  async post<TBody>(
+    url: string,
+    body: TBody,
+    headers?: { Authorization?: string }
+  ) {
+    return fetch(this.baseUrl + url, {
+      method: "POST",
+      headers: {
+        ...this.headers,
+        ...headers,
+      },
+      body: JSON.stringify(body),
+      credentials: "include",
+    });
+  }
+  async put<TBody>(
+    url: string,
+    body: TBody,
+    headers?: { Authorization?: string }
+  ) {
+    return fetch(this.baseUrl + url, {
+      method: "PUT",
+      headers: {
+        ...this.headers,
+        ...headers,
+      },
+      body: JSON.stringify(body),
+      credentials: "include",
+    });
+  }
+  async delete(url: string, headers?: { Authorization?: string }) {
+    return fetch(this.baseUrl + url, {
+      method: "DELETE",
+      headers: {
+        ...this.headers,
+        ...headers,
+      },
+      credentials: "include",
+    });
+  }
+}
+
+const FetchData = new FetchClass();
 
 export const getProducts = async (query: string) => {
-  const res = await Fetch(`/products?query=${query}`);
-  return res.data as IGetProducts;
+  const res = await FetchData.get(`/products?query=${query}`);
+  return (await res.json()) as IGetProducts;
 };
 
 export const getProductsInfinity = async (page: number) => {
-  const res = await Fetch(`/products?page=${page}`);
-  return res.data as IGetProductsInfinity;
+  const res = await FetchData.get(`/products?page=${page}`);
+  return (await res.json()) as IGetProductsInfinity;
 };
 
 export const getProduct = async (id: string) => {
-  const res = await Fetch(`/products/${id}`);
-  return res.data as IGetProduct;
+  const res = await FetchData.get(`/products/${id}`);
+  return (await res.json()) as IGetProduct;
 };
 
 export const getAmountProducts = async () => {
-  const res = await Fetch("/products/amount");
-  return res.data as { msg: string; data: number };
+  const res = await FetchData.get("/products/amount");
+  return (await res.json()) as { msg: string; data: number };
 };
 
 export const getUsers = async () => {
-  const res = await Fetch("/users");
-  return res.data as IGetUsers;
+  const res = await FetchData.get("/users");
+  return (await res.json()) as IGetUsers;
 };
 
 export const getOrders = async (query: string, authtoken?: string) => {
   if (authtoken && !token) token = authtoken;
-  const res = await Fetch(`/orders?query=${query}`);
-  return res.data as IGetOrders;
+  const res = await FetchData.get(`/orders?query=${query}`);
+  return (await res.json()) as IGetOrders;
 };
 
 export const getAmountOrders = async (authtoken?: string) => {
   if (authtoken && !token) token = authtoken;
-  const res = await Fetch("orders/amount");
-  return res.data as { msg: string; data: number };
+  const res = await FetchData.get("/orders/amount");
+  return (await res.json()) as { msg: string; data: number };
 };
 
 export const getAmountOrdersPending = async (authtoken?: string) => {
   if (authtoken && !token) token = authtoken;
-  const res = await Fetch(`orders/amount?status=pending`);
-  return res.data as { msg: string; data: number };
+  const res = await FetchData.get(`/orders/amount?status=pending`);
+  return (await res.json()) as { msg: string; data: number };
 };
 
 export const getRevenue = async () => {
-  const res = await Fetch("orders/revenue");
-  return res.data as { msg: string; data: number };
+  const res = await FetchData.get("/orders/revenue");
+  return (await res.json()) as { msg: string; data: number };
 };
 
 export const getSales = async (year: number, authtoken?: string) => {
   if (authtoken && !token) token = authtoken;
-  const res = await Fetch(`orders/sales_by_months/${year}`);
-  return res.data as {
+  const res = await FetchData.get(`/orders/sales_by_months/${year}`);
+  return (await res.json()) as {
     msg: string;
     data: { _id: { month: number }; totalSales: number }[];
   };
 };
 
 export const updateOrder = async (id: string, data: IOrderUpdate<IOrder>) => {
-  const res = await Fetch.put("/orders/" + id, data);
-  return res.data as IGetOrder;
+  const res = await FetchData.put("/orders/" + id, data);
+  return (await res.json()) as IGetOrder;
 };
 
 export const getReviews = async (
   value: "has been checked" | "has not been checked"
 ) => {
-  const res = await Fetch(
+  const res = await FetchData.get(
     "/reviews" + "?checked=" + (value === "has been checked" ? true : false)
   );
-  return res.data as { msg: string; data: IReview[] };
+  return (await res.json()) as { msg: string; data: IReview[] };
 };
 
 export const getReviewsInfinity = async (page: number) => {
-  const res = await Fetch(`/reviews?checked=false&page=${page}`);
-  return res.data as {
+  const res = await FetchData.get(`/reviews?checked=false&page=${page}`);
+  return (await res.json()) as {
     msg: string;
     data: { lastPage: number; nextPage: number; reviews: IReview[] };
   };
 };
-
-const checkauth_url = process.env.NEXT_PUBLIC_CHECKAUTH_URL || "";
-export async function checkauth(
-  authtoken: string | undefined
-): Promise<{ isadmin: boolean }> {
-  if (authtoken) {
-    const res = await fetch(checkauth_url, {
-      headers: {
-        Authorization: `Bearer ${authtoken}`,
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    });
-
-    if (!res.ok) return { isadmin: false };
-    return res.json();
-  }
-  return { isadmin: false };
-}
